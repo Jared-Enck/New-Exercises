917b4422c714d0db7974f540310be9b64b6214d6
Jared Enck <jared@DESKTOP-GRBT38R.localdomain>
2022-08-30 18:09:40 -0400

    completed ex files

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -1,1 +1,29 @@
-console.log("Let's get this party started!");
+const $container = $(".gif-container");
+const $input = $("#gif-category");
+const api_key = "E3WXqK7mBNPuvw97JK37Lv53Fm3f0Axe";
+
+function createGif(res) {
+  const resArrLength = res.data.length;
+  if (resArrLength) {
+    const randoIdx = Math.floor(Math.random() * resArrLength);
+    const $gifDiv = $("<div>");
+    const $gif = $("<img>", { src: res.data[randoIdx].images.original.url });
+    $gifDiv.append($gif);
+    $container.append($gifDiv);
+  }
+}
+
+$("form").on("submit", async function (e) {
+  e.preventDefault();
+  let search = $input.val();
+  const res = await axios.get("http://api.giphy.com/v1/gifs/search", {
+    params: { q: search, api_key },
+  });
+  console.log(res);
+  createGif(res.data);
+  $input.val("");
+});
+
+$("#remove").on("click", () => {
+  $container.empty();
+});
