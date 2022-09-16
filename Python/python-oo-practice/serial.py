85102a5f6f4532507d190a014db093ca04cd9652
Jared Enck <jared@DESKTOP-GRBT38R.localdomain>
2022-09-16 11:43:50 -0400

    completed ex files

--- Python/python-oo-practice/serial.py
+++ Python/python-oo-practice/serial.py
@@ -19,4 +19,24 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start):
+        """Create serial number starting at 100"""
+        self.start = start
+        self.first_instance = True
+
+    def generate(self):
+        """Generate new serial number incrementing from start"""
+        if self.first_instance:
+            self.first_instance = False
+            return self.start
+
+        self.start += 1
+        return self.start
+
+    def reset(self):
+        """Resets serial number to start value 100"""
+        self.start = 100
+        self.first_instance = True
+
+
 
--- Python/python-oo-practice/wordfinder.py
+++ Python/python-oo-practice/wordfinder.py
@@ -1,5 +1,65 @@
 """Word Finder: finds random words from a dictionary."""
 
+from random import choice
 
 class WordFinder:
-    ...
+    """    
+        >>> wf = WordFinder("words.txt")
+        235886
+
+        >>> rando_word = wf.rando_word()
+
+        >>> isinstance(rando_word, str)
+        True
+
+    """
+    def __init__(self, file):
+        """Calls read_file func on passed file"""
+        self.opened_file = open(file, 'r')
+        self.words = []
+        self.read_file()
+        print(len(self.words))
+
+    def read_file(self):
+        """Read file. Clean and append each word to list"""
+
+        for line in self.opened_file:
+            clean_line = line.strip()
+            self.words.append(clean_line)
+
+    def rando_word(self):
+        """Returns random word from list of words"""
+
+        return choice(self.words)
+
+
+class SpecialWordFinder(WordFinder):
+    """Removes comment lines and blank lines
+
+    >>> spw = SpecialWordFinder('comments.txt')
+    4
+
+    >>> spw.rando_word() in ['kale','parsnips','apple','mango']
+    True
+
+    >>> spw.rando_word() in ['kale','parsnips','apple','mango']
+    True
+
+    >>> spw.rando_word() in ['kale','parsnips','apple','mango']
+    True
+
+    >>> spw.rando_word() in ['kale','parsnips','apple','mango']
+    True
+
+    """
+    def __init__(self, file):
+        super().__init__(file)
+
+    def read_file(self):
+        """Read and also filter out comments/blank lines. Then append word to list.
+        
+        """
+        for line in self.opened_file:
+            clean_line = line.strip()
+            if clean_line and not line.startswith("#"):
+                self.words.append(clean_line)
