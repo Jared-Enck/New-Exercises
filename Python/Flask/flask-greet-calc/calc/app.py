67a07e2347454c919069e32e5c88becb9634d7e0
Jared Enck <jared@DESKTOP-GRBT38R.localdomain>
2022-09-16 18:34:14 -0400

    completed ex files

--- Python/Flask/flask-greet-calc/calc/app.py
+++ Python/Flask/flask-greet-calc/calc/app.py
@@ -1,1 +1,45 @@
 # Put your app in here.
+from flask import Flask, request
+from operations import *
+
+app = Flask(__name__)
+
+@app.route('/add')
+def add_query():
+    
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+
+    total = add(a,b)
+
+    return str(total)
+
+@app.route('/sub')
+def sub_query():
+
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+
+    total = sub(a,b)
+
+    return str(total)
+
+@app.route('/mult')
+def multiply_query():
+
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+
+    total = mult(a,b)
+
+    return str(total)
+
+@app.route('/div')
+def divide_query():
+
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+
+    total = div(a,b)
+
+    return str(total)
