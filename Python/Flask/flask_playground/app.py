95d6f36d0e7a6fc9651bfd9ce3d77a214e6a3c50
Jared Enck <jared@DESKTOP-GRBT38R.localdomain>
2022-09-19 14:11:37 -0400

    added templates

--- Python/Flask/flask_playground/app.py
+++ Python/Flask/flask_playground/app.py
@@ -3,37 +3,20 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 
+app.debug = True
+
 app.config['SECRET_KEY'] = 'ughmorechickens1234'
+
 debug = DebugToolbarExtension(app)
 
 @app.route('/')
 def home_page():
     """Shows home page"""
 
-    html = """
-    <html>
-        <body>
-            <h1>Home Page</h1>
-            <p>Welcome to my simple app!</p>
-            <a href="/about">About Us</a>
-        </body>
-    </html>
-    """
-
-    return html
+    return render_template('home.html')
 
 @app.route('/about')
 def about_page():
     """Shows about page"""
-    
-    html = """
-    <html>
-        <body>
-            <h1>About Page</h1>
-            <p>We're awesome! That is all. Please go about your day.</p>
-            <a href="/">Go Home</a>
-        </body>
-    </html>
-    """
 
-    return html
+    return render_template('about.html')
