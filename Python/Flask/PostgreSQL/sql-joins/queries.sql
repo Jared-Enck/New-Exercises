f54354918c4a0f9bef1f708f2350c75bfab8c30d
Jared Enck <jaredenck86@gmail.com>
2022-10-05 14:12:25 -0400

    completed ex files

--- Python/Flask/PostgreSQL/sql-joins/queries.sql
+++ Python/Flask/PostgreSQL/sql-joins/queries.sql
@@ -1,1 +1,18 @@
--- write your queries here
+--Query1
+    SELECT * FROM owners o
+    LEFT JOIN vehicles v
+    ON o.id = v.owner_id;
+--Query2
+    SELECT first_name, last_name, COUNT(*) AS count FROM owners o
+    JOIN vehicles v
+    ON o.id = v.owner_id
+    GROUP BY first_name, last_name
+    ORDER BY first_name;
+--Query3
+    SELECT first_name, last_name, ROUND(AVG(price)) AS average_price, COUNT(*) AS count 
+    FROM owners o
+    JOIN vehicles v
+    ON o.id = v.owner_id
+    GROUP BY first_name, last_name
+    HAVING ROUND(AVG(price)) > 10000 AND COUNT(*) > 1
+    ORDER BY first_name DESC;
