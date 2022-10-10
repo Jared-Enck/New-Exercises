82522fb10abf7e3d954f04b5dc09ed6ca854a3f9
Jared Enck <jaredenck86@gmail.com>
2022-10-10 18:01:22 -0400

    added repr method to Pet model

--- Python/Flask/PostgreSQL/SQLAlchemy/models.py
+++ Python/Flask/PostgreSQL/SQLAlchemy/models.py
@@ -13,10 +13,14 @@ class Pet(db.Model):
     """Pet."""
     __tablename__ = 'pets'
     
+    def __repr__(self):
+        p = self
+        return f'<Pet id={p.id} name={p.name} species={p.species} hunger={p.hunger}'
+    
     id = db.Column(db.Integer, primary_key=True, autoincrement= True)
-    
+
     name = db.Column(db.String(50), nullable=False, unique=True)
-    
+
     species = db.Column(db.String(30), nullable=True)
-    
+
     hunger = db.Column(db.Integer, nullable=False, default=20)
