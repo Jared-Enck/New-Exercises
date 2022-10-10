4d83b7424bf91bedfe279835e64c8c9ff86ea8af
Jared Enck <jaredenck86@gmail.com>
2022-10-10 17:01:07 -0400

    added import Pet from models

--- Python/Flask/PostgreSQL/SQLAlchemy/app.py
+++ Python/Flask/PostgreSQL/SQLAlchemy/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db
+from models import db, connect_db, Pet
 
 app = Flask(__name__)
 debug = True
