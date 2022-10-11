0160c3720b8ec250a2aa1aab7614c2e3da691c64
Jared Enck <jaredenck86@gmail.com>
2022-10-11 12:32:39 -0400

    added requirements

--- Python/Flask/PostgreSQL/flask-blogly/app.py
+++ Python/Flask/PostgreSQL/flask-blogly/app.py
@@ -1,12 +1,22 @@
 """Blogly application."""
 
-from flask import Flask
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db
 
 app = Flask(__name__)
+debug = True
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'chickensrdumb4242'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
-db.create_all()
+
+app.route('/')
+def home_page():
+    """Shows home page"""
+    return render_template('base.html')
