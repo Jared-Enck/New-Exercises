ca9b2e604d3f2f32f502bb919bd0f21d0bf111c3
Jared Enck <jaredenck86@gmail.com>
2022-10-10 20:55:54 -0400

    added details page, add pet form

--- Python/Flask/PostgreSQL/SQLAlchemy/app.py
+++ Python/Flask/PostgreSQL/SQLAlchemy/app.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, Pet
@@ -15,6 +16,32 @@ toolbar = DebugToolbarExtension(app)
 connect_db(app)
 
 @app.route('/')
-def home_page():
-    """Shows home page"""
-    return render_template('base.html')
+def list_pets():
+    """Shows list of all pets in db"""
+    pets = Pet.query.all()
+    return render_template('list.html', pets=pets)
+
+@app.route('/', methods=['POST'])
+def create_pet():
+    """Add pet to list in db"""
+    name = request.form['name']
+    species = request.form['species']
+    hunger = request.form['hunger']
+    hunger = int(hunger) if hunger else None
+    
+    new_pet = Pet(name=name, species=species, hunger=hunger)
+    db.session.add(new_pet)
+    db.session.commit()
+    
+    return redirect(f'/{new_pet.id}')
+
+@app.route('/<int:pet_id>')
+def show_pet(pet_id):
+    """Show details about pet"""
+    pet = Pet.query.get_or_404(pet_id)
+    return render_template('details.html', pet=pet)
+
+@app.route('/species/<species_id>')
+def show_pets_by_species(species_id):
+    pets = Pet.get_by_species(species_id)
+    return render_template('species.html', pets = pets, species=species_id)
