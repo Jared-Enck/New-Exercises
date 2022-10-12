73dfcedfc9fb9e0bd8c4f577e426559c2cfd68c8
Jared Enck <jaredenck86@gmail.com>
2022-10-12 17:36:41 -0400

    setup db tables

---
+++ Python/Flask/PostgreSQL/employee_db_ex/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Department, Employee
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///employees_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'chickensrdumb4242'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
+
