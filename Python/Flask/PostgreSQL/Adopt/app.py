b61fd292b330727e68ff4f9575a5f92a94454110
Jared Enck <jaredenck86@gmail.com>
2022-10-19 16:27:51 -0400

    created home page route for base template and models file

---
+++ Python/Flask/PostgreSQL/Adopt/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, redirect, render_template, request, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///pet_adoption_agency'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'chickensrdumb4242'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
+
+@app.route('/')
+def show_homepage():
+    """Shows home page for pet list"""
+    
+    return render_template('base.html')
---
+++ Python/Flask/PostgreSQL/Adopt/models.py
@@ -0,0 +1,8 @@
+import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
