f728821de3bf18652b76f053cf789947f24ba93b
Jared Enck <jaredenck86@gmail.com>
2022-10-21 13:44:21 -0400

    demo files

---
+++ Python/Flask/itunes-api/app.py
@@ -0,0 +1,10 @@
+import requests
+
+term = 'Star Wars'
+
+res = requests.get('https://itunes.apple.com/search', params={'term': term, 'limit': 5})
+
+data = res.json()
+
+for result in data['results']:
+    print(result['trackName'])
