321e3558a0d650bdf12363b6742c014763e7068c
Jared Enck <jaredenck86@gmail.com>
2022-10-21 17:22:06 -0400

    setup code along

--- Python/Flask/map-quest-api-demo/app.py
+++ Python/Flask/map-quest-api-demo/app.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
 import requests
-from secrets import mapquest_API_key
+from secrets import MQ_API_KEY
 
 API_BASE_URL = 'http://www.mapquestapi.com/geocoding/v1/address'
 
 app = Flask(__name__)
 
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///***'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'chickensrdumb4242'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+toolbar = DebugToolbarExtension(app)
+
 @app.route('/')
+def homepage():
+    """Shows homepage"""
+    
+    return render_template('base.html')
+
+@app.route('/add_address')
 def show_address_form():
+    """Shows address form"""
+    
     return render_template('address_form.html')
--- Python/Flask/map-quest-api-demo/mapquest.py
+++ Python/Flask/map-quest-api-demo/mapquest.py
@@ -1,7 +1,7 @@
-import requests
-from secrets import mapquest_API_key
+# import requests
+# from secrets import mapquest_API_key
 
-API_BASE_URL = 'http://www.mapquestapi.com/geocoding/v1/address'
+# API_BASE_URL = 'http://www.mapquestapi.com/geocoding/v1/address'
 
-res = requests.get(API_BASE_URL, 
-                   params={'key': mapquest_API_key, 'location': 'Jacksonville FL'})
+# res = requests.get(API_BASE_URL, 
+#                    params={'key': mapquest_API_key, 'location': 'Jacksonville FL'})
