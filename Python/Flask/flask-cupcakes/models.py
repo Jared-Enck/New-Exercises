33778b77146952c9c6424f48baa08def2d6c887e
Jared Enck <jaredenck86@gmail.com>
2022-10-23 11:40:31 -0400

    created cupcake Model and updated app file

--- Python/Flask/flask-cupcakes/models.py
+++ Python/Flask/flask-cupcakes/models.py
@@ -1,1 +1,28 @@
 """Models for Cupcake app."""
+from ctypes import sizeof
+from logging.handlers import RotatingFileHandler
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+    
+default_img = 'https://tinyurl.com/demo-cupcake'
+
+class Cupcake(db.Model):
+    """Create a cupcake"""
+    
+    __tablename__ = 'cupcakes'
+    
+    id = db.Column(db.Integer, 
+                            primary_key=True, autoincrement=True)
+    flavor = db.Column(db.String(20), 
+                            nullable=False)
+    size = db.Column(db.String(20), 
+                            nullable=False)
+    rating = db.Column(db.Float, 
+                            nullable=False)
+    image = db.Column(db.String, 
+                            nullable=False, default=default_img)
