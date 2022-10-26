887e279d96e217e70dbebc803e79b4681c057942
Jared Enck <jaredenck86@gmail.com>
2022-10-26 13:05:22 -0400

    created homepage route and User Model

--- Python/Flask/auth_demo/app.py
+++ Python/Flask/auth_demo/app.py
@@ -6,7 +6,7 @@ from models import db, connect_db
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///auth_demo'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = 'chickensrdumb4242'
@@ -14,6 +14,7 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
+db.create_all()
 
 @app.route('/')
 def show_homepage():
--- Python/Flask/auth_demo/models.py
+++ Python/Flask/auth_demo/models.py
@@ -5,3 +5,13 @@ db = SQLAlchemy()
 def connect_db(app):
     db.app = app
     db.init_app(app)
+    
+class User(db.Model):
+    
+    __tablename__ = 'users'
+    
+    id= db.Column(db.Integer, primary_key=True, autimcrement=True)
+    
+    username = db.Column(db.Text, nullable=False, unique=True)
+    
+    password = db.Column(db.Text, nullable=False)   
