233ee6adece5f0b5014d164f94e7052c49b5ad0e
Jared Enck <jaredenck86@gmail.com>
2022-10-26 12:52:10 -0400

    setup demo files

---
+++ Python/Flask/auth_demo/app.py
@@ -0,0 +1,16 @@
+from flask_cors import CORS
+from flask import Flask, redirect, render_template, request, flash, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db
+
+app = Flask(__name__)
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'chickensrdumb4242'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
