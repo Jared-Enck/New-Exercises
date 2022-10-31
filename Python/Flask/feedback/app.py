e188ebc1746895a4424519a1401ede7423b13d18
Jared Enck <jaredenck86@gmail.com>
2022-10-31 13:24:17 -0400

    created register html and added styles

--- Python/Flask/feedback/app.py
+++ Python/Flask/feedback/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect, render_template, request, flash, jsonify, session
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db
+from models import db, connect_db, User
+from forms import UserForm
 
 app = Flask(__name__)
 
@@ -13,3 +14,19 @@ toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
+
+@app.route('/')
+def redirect_register():
+    """Redirects to register form"""
+    
+    return redirect('/register')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_user():
+    """Shows or handles register form"""
+    
+    form = UserForm()
+    
+    
+    
+    return render_template('register.html', form=form)
---
+++ Python/Flask/feedback/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextAreaField, EmailField
+from wtforms.validators import InputRequired, Email, Length, Optional
+
+class UserForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=2,max=20)])
+    password = PasswordField('Password', validators=[InputRequired()])
+    email = EmailField('Email', validators=[InputRequired(), Length(max=50),Email(message=('Enter valid email.'))])
+    first_name = StringField('First Name', validators=[Optional()])
+    last_name = StringField('Last Name', validators=[Optional()])
