aa5ebff4959f212a99bdcc12240d3a1c7e3c2eee
Jared Enck <jaredenck86@gmail.com>
2022-11-01 21:50:12 -0400

    add os for eviron vars

--- Python/Flask/feedback/app.py
+++ Python/Flask/feedback/app.py
@@ -4,13 +4,14 @@ from flask import Flask, redirect, render_template, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from models import Feedback, db, connect_db, User
 from forms import LoginForm, UserForm, AddFeedback
+import os
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///feedback_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SECRET_KEY'] = 'chickensrdumb4242'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'chickensarestilldumb4242')
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 toolbar = DebugToolbarExtension(app)
 
