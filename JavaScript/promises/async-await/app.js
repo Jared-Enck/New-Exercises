9e4d6838713741b95bcfed828bb5a78ca0315bef
Jared Enck <jaredenck86@gmail.com>
2023-01-13 17:40:06 -0500

    completed ex files

--- JavaScript/promises/async-await/app.js
+++ JavaScript/promises/async-await/app.js
@@ -36,3 +36,56 @@ async function fourFactsForOne(num) {
     }
     factsHTML(factsArr)
 }
+
+const CARDS_API = 'https://deckofcardsapi.com/api/deck/'
+
+async function newDeckAndDraw() {
+    let {data} = await axios.get(`${CARDS_API}/new/draw/?count=1`)
+    console.log(`${data.cards[0].value} of ${data.cards[0].suit}`)
+}
+
+$('#draw').on('click', () => {
+    if (deck.deckId) {
+        if (deck.deckCount > 0) {
+            deck.drawCard()
+        } else {
+            alert('No cards left!')
+        }
+    }
+})
+$('#shuffle').on('click', () => {
+    if (deck.deckId) {
+        deck.shuffle()
+        resContainer.empty()
+    }
+})
+
+cardHTML = (cardURL) => {
+    const cardImg = $('<img>').attr('src',`${cardURL}`)
+    resContainer.append(cardImg)
+}
+
+const deck = {
+    async init() {
+        let {data} = await axios.get(`${CARDS_API}/new/shuffle`)
+        this.deckId = data.deck_id
+        this.deckCount = data.remaining
+    },
+    async shuffle() {
+        let {data} = await axios.get(`${CARDS_API}/${this.deckId}/shuffle/`)
+        this.deckCount = data.remaining
+    },
+    async drawAndAnotherOne() {
+        let {data: c1} = await axios.get(`${CARDS_API}/${this.deckId}/draw/?count=1`)
+        let {data: c2} = await axios.get(`${CARDS_API}/${this.deckId}/draw/?count=1`)
+        console.log(`${c1.cards[0].value} of ${c1.cards[0].suit}`)
+        console.log(`${c2.cards[0].value} of ${c2.cards[0].suit}`)
+    },
+    async drawCard() {
+        let {data} = await axios.get(`${CARDS_API}/${this.deckId}/draw/?count=1`)
+        cardHTML(data.cards[0].image)
+        this.deckCount = data.remaining
+    }
+}
+
+$(deck.init())
