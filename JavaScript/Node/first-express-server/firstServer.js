ea8cd15d722b3f1d3750204842d765435f4a571a
Jared Enck <jaredenck86@gmail.com>
2023-01-17 17:39:55 -0500

    add routes

--- JavaScript/Node/first-express-server/firstServer.js
+++ JavaScript/Node/first-express-server/firstServer.js
@@ -1,12 +1,24 @@
 const express = require('express')
+const res = require('express/lib/response')
 
 const app = express()
 
 // app routes here.
 
+app.get('/', (req, res) => {
+    res.send('<h1>Homepage</h1>')
+})
+
 app.get('/dogs', (req, res) => {
-    console.log('here are dogs.')
     return res.send('Bork Bork!')
+})
+
+app.get('/cats', (req, res) => {
+    res.send('Meow meow.')
+})
+
+app.post('/cats', function createCat(req, res) {
+    res.send('<h2>New cat created.</h2>')
 })
 
 // app.listen at the end. Args (somePort, callback)
