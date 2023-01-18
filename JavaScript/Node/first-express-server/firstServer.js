171bdfec6f51092e6d6fb6577d50efb9ef871489
Jared Enck <jaredenck86@gmail.com>
2023-01-17 19:28:27 -0500

    added err handler and 404 handler

--- JavaScript/Node/first-express-server/firstServer.js
+++ JavaScript/Node/first-express-server/firstServer.js
@@ -1,6 +1,5 @@
 const express = require('express')
-const res = require('express/lib/response')
-
+const ExpressError = require('./expressError')
 const app = express()
 
 app.use(express.json())
@@ -12,8 +11,14 @@ app.get('/', (req, res) => {
     res.send('<h1>Homepage</h1>')
 })
 
-app.get('/dogs', (req, res) => {
-    return res.send('Bork Bork!')
+app.get('/dogs', (req, res, next) => {
+    try {
+        throw new ExpressError("No Dogs Allowed!", 403)
+        return res.send('Bork Bork!')
+    }
+    catch (e) {
+        next(e)
+    }
 })
 
 app.get('/cats', (req, res) => {
@@ -22,6 +27,22 @@ app.get('/cats', (req, res) => {
 
 app.post('/cats', function createCat(req, res) {
     res.send('<h2>New cat created.</h2>')
+})
+
+// custom 404, before err handler, after all routes
+app.use((req, res, next) => {
+    const e = new ExpressError('Page not found.', 404)
+    next(e)
+})
+
+// err handler, before app.listen
+app.use((err, req, res, next) => {
+    let status = err.status || 500
+    let msg = err.msg
+
+    return res.status(status).json({
+        error: {msg, status}
+    })
 })
 
 // app.listen at the end. Args (somePort, callback)
