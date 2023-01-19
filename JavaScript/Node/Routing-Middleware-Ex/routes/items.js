5e2e649d583844e06c1400cc3a7b668fe5f8d1c6
Jared Enck <jaredenck86@gmail.com>
2023-01-19 13:37:31 -0500

    finish routes and add test file

--- JavaScript/Node/Routing-Middleware-Ex/routes/items.js
+++ JavaScript/Node/Routing-Middleware-Ex/routes/items.js
@@ -9,8 +9,11 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res, next) => {
     try {
-        if (!req.body.name || typeof req.body.name !== 'string') {
+        if (!req.body.name) {
             throw new ExpressError('Item must have a name.', 400)
+        }
+        if (typeof req.body.name !== 'string') {
+            throw new ExpressError('Item name must be a string of text.', 400)
         }
         if (typeof req.body.price !== 'number') {
             throw new ExpressError('Price must be a number', 400)
@@ -19,45 +22,58 @@ router.post('/', (req, res, next) => {
         ITEMS.push(newItem)
 
         return res.status(201).json({ added: newItem })
-    } catch (e) {
+    }
+    catch (e) {
         return next(e)
     }
 })
 
 router.get('/:name', (req, res, next) => {
     try {
-        const item = ITEMS.find(i => i.name.toLowerCase() === req.params.name.toLowerCase())
+        const item = ITEMS.find(
+            i => i.name.toLowerCase() === req.params.name.toLowerCase()
+        );
         if (item === undefined) {
             throw new ExpressError('Item not found', 404)
         }
         return res.json({item})
-    } catch (e) {
+    }
+    catch (e) {
         return next(e)
     }
 })
 
 router.patch('/:name', (req, res, next) => {
     try {
-        const item = ITEMS.find(i => i.name.toLowerCase() === req.params.name.toLowerCase())
+        const item = ITEMS.find(
+            i => i.name.toLowerCase() === req.params.name.toLowerCase()
+        );
         if (item === undefined) {
             throw new ExpressError('Item not found', 404)
         }
         item.name = req.body.name
         item.price = req.body.price
+        
         return res.json({ updated: item })
-    } catch (e) {
+    }
+    catch (e) {
         return next(e)
     }
 })
 
 router.delete('/:name', (req, res, next) => {
     try {
-        const item = ITEMS.find(i => i.name.toLowerCase() === req.params.name.toLowerCase())
-        if (item === undefined) {
+        const item = ITEMS.findIndex(
+            i => i.name.toLowerCase() === req.params.name.toLowerCase()
+        );
+        if (item === -1) {
             throw new ExpressError('Item not found', 404)
         }
-        // stuff here
-    } catch (e) {
+        ITEMS.splice(item, 1)
+
+        return res.json({ message: 'Deleted' })
+    } 
+    catch (e) {
         return next(e)
     }
 })
