b9ce5e5b0a7a2911707bdd2e0ce51d8e1e47b65f
Jared Enck <jaredenck86@gmail.com>
2023-01-18 19:00:34 -0500

    add routes get,post,patch,delete

--- JavaScript/Node/Routing-Middleware-Ex/routes/items.js
+++ JavaScript/Node/Routing-Middleware-Ex/routes/items.js
@@ -1,11 +1,66 @@
 const express = require('express')
+const ExpressError = require('../expError')
 const router = new express.Router()
 const ITEMS = require('../fakeDB')
 
-router.get('/', (req, res, next) => {
-    return res.json({
-        items: ITEMS
-    })
+router.get('/', (req, res) => {
+    return res.json({ items: ITEMS })
 })
+
+router.post('/', (req, res, next) => {
+    try {
+        if (!req.body.name || typeof req.body.name !== 'string') {
+            throw new ExpressError('Item must have a name.', 400)
+        }
+        if (typeof req.body.price !== 'number') {
+            throw new ExpressError('Price must be a number', 400)
+        }
+        const newItem = { name: req.body.name, price: req.body.price }
+        ITEMS.push(newItem)
+
+        return res.status(201).json({ added: newItem })
+    } catch (e) {
+        return next(e)
+    }
+})
+
+router.get('/:name', (req, res, next) => {
+    try {
+        const item = ITEMS.find(i => i.name.toLowerCase() === req.params.name.toLowerCase())
+        if (item === undefined) {
+            throw new ExpressError('Item not found', 404)
+        }
+        return res.json({item})
+    } catch (e) {
+        return next(e)
+    }
+})
+
+router.patch('/:name', (req, res, next) => {
+    try {
+        const item = ITEMS.find(i => i.name.toLowerCase() === req.params.name.toLowerCase())
+        if (item === undefined) {
+            throw new ExpressError('Item not found', 404)
+        }
+        item.name = req.body.name
+        item.price = req.body.price
+        return res.json({ updated: item })
+    } catch (e) {
+        return next(e)
+    }
+})
+
+router.delete('/:name', (req, res, next) => {
+    try {
+        const item = ITEMS.find(i => i.name.toLowerCase() === req.params.name.toLowerCase())
+        if (item === undefined) {
+            throw new ExpressError('Item not found', 404)
+        }
+        // stuff here
+    } catch (e) {
+        return next(e)
+    }
+})
+
 
 module.exports = router
