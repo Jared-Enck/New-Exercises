d15c0659a6cfbf8491567b23583b72d9850b3da4
Jared Enck <jaredenck86@gmail.com>
2023-01-24 18:59:39 -0500

    add routes post,patch,delete

--- JavaScript/Node/Node-pg/routes/users.js
+++ JavaScript/Node/Node-pg/routes/users.js
@@ -2,10 +2,59 @@ const express = require('express')
 const router = express.Router()
 const db = require('../db')
 
-router.get('/', async function (req, res) {
-    const results = await db.query(`SELECT * FROM users`)
+router.get('/', async (req, res, next) => {
+    try {
+        const results = await db.query(`SELECT * FROM users`)
+        return res.json(results.rows)
+    }
+    catch (e) {
+        return next(e)
+    }
+})
 
-    return res.json(results.rows)
+router.get('/search', async (req, res, next) => {
+    try {
+        const {type} = req.query
+        const results = await db.query(`SELECT * FROM users WHERE type ='${type}'`)
+        return res.json(results.rows)
+    }
+    catch (e) {
+        return next(e) 
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try {
+        const { name, type } = req.body
+        const results = await db.query('INSERT INTO users (name, type) VALUES ($1, $2) RETURNING id, name, type', [name,type])
+        return res.status(201).json(results.rows[0])
+    }
+    catch (e) {
+        return next(e)
+    }
+})
+
+router.patch('/:id', async (req, res, next) => {
+    try {
+        const {id} = req.params
+        const { name, type } = req.body
+        const results = await db.query('UPDATE users SET name=$1, type=$2 WHERE id=$3 RETURNING id, name, type', [name,type,id])
+        return res.send(results.rows[0])
+    }
+    catch (e) {
+        next(e)
+    }
+})
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        const {id} = req.params
+        const results = db.query('DELETE FROM users WHERE id = $1', [id])
+        return res.send({msg: 'Deleted.'})
+    }
+    catch (e) {
+        next(e)
+    }
 })
 
 module.exports = router;
