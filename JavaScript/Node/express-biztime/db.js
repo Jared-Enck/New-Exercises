bd9a7d67dbb77b2d048f6ba869050d5445f67ef8
Jared Enck <jaredenck86@gmail.com>
2023-01-25 18:43:20 -0500

    complete ex files

--- JavaScript/Node/express-biztime/db.js
+++ JavaScript/Node/express-biztime/db.js
@@ -1,2 +1,22 @@
 /** Database setup for BizTime. */
+const { PGPASSWORD } = require('./instance/secrets')
 
+const { Client } = require("pg");
+
+let dbase;
+
+if (process.env.NODE_ENV === "test") {
+  dbase = 'biztime_test';
+} else {
+  dbase = 'biztime';
+}
+
+const connectionString = `postgresql://jared:${PGPASSWORD}/${dbase}`
+
+const db = new Client({
+  connectionString,
+});
+
+db.connect()
+
+module.exports = db;
