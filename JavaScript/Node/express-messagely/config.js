86f1e5c07f02196637f21a9541a05e9b5aba9d92
Jared Enck <jaredenck86@gmail.com>
2023-02-08 15:43:05 -0500

    update DB_URI to use socket connect

--- JavaScript/Node/express-messagely/config.js
+++ JavaScript/Node/express-messagely/config.js
@@ -1,17 +1,16 @@
 /** Common config for message.ly */
 
 // read .env files and make environmental variables
-
 require("dotenv").config();
+const env = process.env
 
-const DB_URI = (process.env.NODE_ENV === "test")
-  ? "postgresql:///messagely_test"
-  : "postgresql:///messagely";
+const DB_URI = (env.NODE_ENV === "test")
+  ? `socket:/var/run/postgresql?db=${env.DB_NAME_TEST}`
+  : `socket:/var/run/postgresql?db=${env.DB_NAME}`;
 
-const SECRET_KEY = process.env.SECRET_KEY || "secret";
+const SECRET_KEY = env.SECRET_KEY;
 
-const BCRYPT_WORK_FACTOR = 12;
-
+const BCRYPT_WORK_FACTOR = (env.NODE_ENV === "test") ? 1 : 12;
 
 module.exports = {
   DB_URI,
