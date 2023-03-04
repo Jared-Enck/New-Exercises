391d9fbc4ccd024344ea8dd6466d7f3f4eb9111b
Jared Enck <jaredenck86@gmail.com>
2023-03-03 19:01:53 -0500

    update tests

--- JavaScript/React/react-forms-exercises/part-1/src/__tests__/BoxList.test.js
+++ JavaScript/React/react-forms-exercises/part-1/src/__tests__/BoxList.test.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import BoxList from "../BoxList";
-import BOXES from "../boxes"
 
 it("renders without crashing", function() {
-  render(<BoxList boxes={BOXES} />);
+  render(<BoxList />);
 });
 
 it("matches snapshot", function() {
-  const {asFragment} = render(<BoxList boxes={BOXES} />);
+  const {asFragment} = render(<BoxList />);
   expect(asFragment()).toMatchSnapshot();
 });
