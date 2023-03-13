2271a0400e9118f84c14cb01138ceb2914364e23
Jared Enck <jaredenck86@gmail.com>
2023-03-13 17:37:54 -0400

    add color-factory ex files

---
+++ JavaScript/React/react-router-patterns/color-factory/src/AllRoutes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom'
+
+function AllRoutes() {
+    return (
+        <Routes>
+            <Route 
+                path='/colors'
+            />
+        </Routes>
+    )
+}
+
+export default AllRoutes
---
+++ JavaScript/React/react-router-patterns/color-factory/src/App.js
@@ -0,0 +1,14 @@
+import './styles/App.css';
+import Navbar from './Navbar';
+import AllRoutes from './AllRoutes';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <AllRoutes />
+    </div>
+  );
+}
+
+export default App;
