9030b39689408826ef1d4b99201bdaeb2609c242
Jared Enck <jaredenck86@gmail.com>
2023-04-05 16:16:03 -0400

    binarySearch challenge

---
+++ DataS-Algorithms/binarySearch.js
@@ -0,0 +1,20 @@
+function binarySearch(arr, val) {
+  let leftIdx = 0
+  let rightIdx = arr.length - 1
+
+  while(leftIdx <= rightIdx) {
+    let middleIdx = Math.floor((leftIdx + rightIdx) / 2)
+    let middleVal = arr[middleIdx]
+
+    if (middleVal < val) {
+      leftIdx = middleIdx +1
+
+    } else if (middleIdx > val) {
+      rightIdx = middleIdx -1
+
+    } else {
+      return middleIdx
+    }
+  }
+  return -1
+}
