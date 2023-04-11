74bcd8928d074819d8e85e53d68576c08a5e6eaf
Jared Enck <jaredenck86@gmail.com>
2023-04-11 15:44:13 -0400

    completed ex files

--- DataS-Algorithms/dsa-recursion/recursion.js
+++ DataS-Algorithms/dsa-recursion/recursion.js
@@ -31,33 +31,61 @@ function everyOther(str, newStr = '', i = 0) {
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
-function isPalindrome(str) {
+function isPalindrome(str, reverse = '', i = str.length - 1) {
+  if (i < 0) return str === reverse
 
+  reverse += str[i]
+
+  return isPalindrome(str, reverse, i - 1)
 }
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
-function findIndex(arr, val) {
+function findIndex(arr, val, i = 0) {
+  if (i === arr.length || arr[i] === val) {
+    return arr[i] === val ? i : -1
+  }
 
+  return findIndex(arr, val, i + 1)
 }
 
 /** revString: return a copy of a string, but in reverse. */
 
-function revString(str) {
+function revString(str, reverse = '', i = str.length - 1) {
+  if (i < 0) return reverse
 
+  reverse += str[i]
+
+  return revString(str, reverse, i - 1)
 }
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
-function gatherStrings(obj) {
-
+function gatherStrings(obj, strArr = []) {
+  for (let key in obj) {
+    if (typeof obj[key] === 'object') {
+      gatherStrings(obj[key], strArr)
+    }
+    if (typeof obj[key] === 'string') strArr.push(obj[key])
+  }
+  return strArr
 }
 
 /** binarySearch: given a sorted array of numbers, and a value,
  * return the index of that value (or -1 if val is not present). */
 
-function binarySearch(arr, val) {
+function binarySearch(arr, val, leftIdx = 0, rightIdx = arr.length) {
+  if (leftIdx > rightIdx) return -1
 
+  let middleIdx = Math.floor((leftIdx + rightIdx) / 2)
+  let middleVal = arr[middleIdx]
+
+  if (middleVal === val) return middleIdx
+  
+  if (middleVal < val) {
+    return binarySearch(arr, val, middleIdx + 1, rightIdx)
+  } 
+  return binarySearch(arr, val, leftIdx, middleIdx - 1)
 }
 
 module.exports = {
