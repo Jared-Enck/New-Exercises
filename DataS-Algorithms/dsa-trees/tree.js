0b07c16bc6d0e2db31052842ce915d0ca8aeb634
Jared Enck <jaredenck86@gmail.com>
2023-04-12 15:40:19 -0400

    completed tree.js

--- DataS-Algorithms/dsa-trees/tree.js
+++ DataS-Algorithms/dsa-trees/tree.js
@@ -14,21 +14,56 @@ class Tree {
 
   /** sumValues(): add up all of the values in the tree. */
 
-  sumValues() {
-    
+  sumValues(total = 0, toVisitStack = [this.root]) {
+    if (!this.root) return 0;
+    let current = toVisitStack.pop();
+    // Base case
+    if (!current) return total;
+
+    total += current.val
+
+    for (let child of current.children) {
+      toVisitStack.push(child)
+    }
+    return this.sumValues(total, toVisitStack)
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
-  countEvens() {
+  countEvens(totalEvenNums = 0, toVisitStack = [this.root]) {
+    if (!this.root) return 0;
+    let current = toVisitStack.pop();
+    // Base case.
+    if (!current) return totalEvenNums;
 
+    if (current.val % 2 === 0) totalEvenNums ++;
+
+    for (let child of current.children) {
+      toVisitStack.push(child)
+    }
+
+    return this.countEvens(totalEvenNums, toVisitStack)
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
-  numGreater(lowerBound) {
+  numGreater(
+    lowerBound, 
+    totalGreater = 0, 
+    toVisitStack = [this.root]
+  ) {
+    if (!this.root) return 0;
+    let current = toVisitStack.pop();
+    // Base case.
+    if (!current) return totalGreater;
 
+    if (current.val > lowerBound) totalGreater ++;
+
+    for (let child of current.children) {
+      toVisitStack.push(child)
+    }
+    return this.numGreater(lowerBound, totalGreater, toVisitStack)
   }
 }
 
