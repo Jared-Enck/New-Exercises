8837dd84695f7db1c2c0b19fde5dfe5f125d5b0b
Jared Enck <jaredenck86@gmail.com>
2023-04-18 18:41:28 -0400

    solved average-pair

--- Problem-Solving/prob-freq-count-pointers/average-pair.js
+++ Problem-Solving/prob-freq-count-pointers/average-pair.js
@@ -1,3 +1,16 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(arr, avg) {
+  let left = 0
+  let right = arr.length -1
+
+  while (left < right) {
+    let currAverage = arr[left] + arr[right] / 2
+    if (currAverage === avg) return true
+    else if (currAverage > avg){
+      right --
+    } else {
+      left ++
+    }
+  }
+  return false
 }
