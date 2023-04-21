d9b8c38ec05d0326deda4f208e6212dacba185c5
Jared Enck <jaredenck86@gmail.com>
2023-04-21 15:14:53 -0400

    solve guessing game

--- JavaScript/outcomes-js-tricky-parts/guessing-game.js
+++ JavaScript/outcomes-js-tricky-parts/guessing-game.js
@@ -1,5 +1,22 @@
 function guessingGame() {
-
+  const target = Math.floor(Math.random() * 100);
+  let count = 0;
+  let gameWon = false;
+  
+  return function checkGuess(num) {
+    count ++;
+    if (num === target) {
+      if(!gameWon) {
+        gameWon = true
+        return `You win! You found ${target} in ${count} guesses.`;
+      } else {
+        return 'The game is over, you already won!';
+      }
+    } 
+    return num > target
+      ? `${num} is too high!` 
+      : `${num} is too low!`;
+  }
 }
 
 module.exports = { guessingGame };
