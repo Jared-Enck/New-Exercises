4b7c91774a006cb6dedc53352f8a4cef163801c3
Jared Enck <jaredenck86@gmail.com>
2023-04-21 16:08:07 -0400

    solved create account

--- JavaScript/outcomes-js-tricky-parts/create-account.js
+++ JavaScript/outcomes-js-tricky-parts/create-account.js
@@ -1,5 +1,30 @@
-function createAccount(pin, amount) {
-
+function createAccount(pin, amount = 0) {
+  this.pin = pin;
+  this.amount = amount;
 }
+
+createAccount.prototype.checkBalance = function (pin) {
+  if (pin !== this.pin) return 'Invalid PIN.';
+  return `$${this.amount}`
+}
+createAccount.prototype.deposit = function (pin, deposit) {
+  if (pin !== this.pin) return 'Invalid PIN.';
+  this.amount += deposit;
+  return `Successfully deposited $${deposit}. Current balance: $${this.amount}.`
+}
+createAccount.prototype.withdraw = function (pin, withdraw) {
+  if (pin !== this.pin) return 'Invalid PIN.';
+  if (withdraw > this.amount) {
+    return 'Withdrawal amount exceeds account balance. Transaction cancelled.'
+  }
+  this.amount -= withdraw;
+  return `Successfully withdrew $${withdraw}. Current balance: $${this.amount}.`
+}
+createAccount.prototype.changePin = function (pin, newPin) {
+  if (pin !== this.pin) return 'Invalid PIN.';
+  this.pin = newPin
+  return 'PIN successfully changed!'
+}
+
 
 module.exports = { createAccount };
